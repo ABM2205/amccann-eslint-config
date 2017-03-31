60235e1190538294b6d15df8e1fa07f7503cb51c
Unknown <abm2205@gmail.com>
2017-03-31 11:55:05 -0400

    updated rules

--- eslint-rules/index.js
+++ eslint-rules/index.js
@@ -1,13 +1,13 @@
 module.exports = {
-  extends: [
-    './best-practices.js',
-    './errors.js',
-    './node.js',
-    './style.js',
-    './variables.js',
-    './es6.js',
-  ],
-  rules: {
-    strict: 'error',
-  },
+	extends: [
+		'./best-practices.js',
+		'./errors.js',
+		'./node.js',
+		'./style.js',
+		'./variables.js',
+		'./es6.js',
+	],
+	rules: {
+		strict: 'error',
+	},
 };
